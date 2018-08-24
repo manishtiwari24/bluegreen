22c0349f4fb7b4979bd788209f8970f4c7445751
manishtiwari24 <39103906+manishtiwari24@users.noreply.github.com>
2018-08-24 09:39:19 -0400

    Update image.php

--- image.php
+++ image.php
@@ -11,7 +11,7 @@ if ( $deployment == 'blue') {
 } elseif ($deployment == 'green')  {
   $color = imagecolorallocate($im, 0, 255, 0);
 } else {
-  $color = imagecolorallocate($im, 0, 0, 255);
+  $color = imagecolorallocate($im, 255, 0, 255);
 }
 
 // Draw a filled rectangle
